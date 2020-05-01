fa3037f61b1f8b5f61577ef296c9b7525d780b25
Matthias <matthiasludwig@me.com>
2020-05-01 22:26:28 +0200

    Added album art to Spotify.js and Track.js

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -20,6 +20,7 @@ class Track extends React.Component {
     render() {
         return (
             <div className="Track">
+                <img className={"Track-Image"} src={this.props.track.albumSrc} alt={this.props.track.album} />
                 <div className="Track-information">
                     <h3>{ this.props.track.name }</h3>
                     <p>{ this.props.track.artist } | { this.props.track.album }</p>
